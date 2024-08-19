92b7590bad1c5553600820bce58aab5f54306485
Yuuki175 <realouise175@gmail.com>
2024-08-19 15:43:22 +0800

    first commit

---
+++ dist/js/slider.js
@@ -0,0 +1,10 @@
+var slider = tns ({
+    container: ".testimonials__slider",
+    items: 1,
+    gutter: 100,
+    slideBy: "page",
+    autoplay: true,
+    controls: false,
+    nav: false,
+    autoplayButtonOutput: false,
+})
